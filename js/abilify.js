17c2b06c54f4c4624c5dfdc699356ac60824c295
yoshimuraYuu <yoshiyuu0310@gmail.com>
2013-02-01 10:46:45 +0900

    add new view: balance.js

--- js/abilify.js
+++ js/abilify.js
@@ -45,7 +45,7 @@ Serzone.action = {};
 					this.currentTable.find("tbody:first").append("<tr></tr>").find("tr:last").append(body);
 				}
 			} else {
-				var table = $("<table><tbody><tr></tr></tbody>").find("tr").append(body).end();
+				var table = $("<table><tbody><tr></tr></tbody></table>").find("tr").append(body).end();
 				$(slide.parent.body).append(table);
 
 				this.currentTable = table;
@@ -160,6 +160,7 @@ Serzone.action = {};
 
 					body.parent().remove();
 					
+					console.log(tr.find("td").length);
 					if (tr.find("td").length == 0) {
 						tr.remove();
 					}
